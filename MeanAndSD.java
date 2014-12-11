a7e26c3d2d8b44e79bb5ef67deba98f0daf224e4
Anne Morrow <anne.moore2008@gmail.com>
2014-12-10 22:01:32 -0700

    Added outliers

--- MeanAndSD.java
+++ MeanAndSD.java
@@ -2,17 +2,19 @@
 compilation:  javac -cp .:stdlib.jar MeanAndSD.java
 execution:  java -cp .:stdlib.jar MeanAndSD [N]
 
-Reads an integer N from the command line and N double values from standard input, then prints their mean and standard deviation.
+Reads an integer N from the command line and N double values from standard input, then prints their mean, standard deviation, and a list of outliers.
 
 $ java -cp .:stdlib.jar MeanAndSD 8
-14.2 2 13.0 4.4 9 20.2 5 11 14.4
-The average is 9.85
-The standard deviation is 2.2716531784727407
+6 2.2 9 13 -9 100 -12 4 
+The average is 14.15
+The standard deviation is        13.4999 
+The outliers are: -9.0	100.0	-12.0
 
-java -cp .:stdlib.jar MeanAndSD 4
+$ java -cp .:stdlib.jar MeanAndSD 4
 5 5 5 5
 The average is 5.0
-The standard deviation is 0.0
+The standard deviation is         0.0000 
+The outliers are: 
 ***************************************************************************************/
 
 public class MeanAndSD {
@@ -31,6 +33,15 @@ public class MeanAndSD {
       }
       double stddev = Math.sqrt(squaresum) / (N - 1);
       StdOut.println("The average is " + average);
-      StdOut.println("The standard deviation is " + stddev);
+      StdOut.printf("The standard deviation is %14.4f \n", stddev);
+      
+      StdOut.print("The outliers are: ");
+      // print values that are further than 1.5 standard deviations from the mean
+      for (int i = 0; i < N; i++) {
+         if (Math.abs(data[i] - average) > Math.abs(1.5 * average)) {
+            StdOut.print(data[i] + "\t");
+         }
+      }
+      StdOut.println();
    }
 }
